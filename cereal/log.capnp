7ad23e269ff7677022d3ada2a4b824af3ae0b0d6
Vehicle Researcher <user@comma.ai>
2017-05-17 00:40:33 -0700

    openpilot v0.3.1 release
    old-commit-hash: c92c1ba9be809e2e5d241d038b720df041e7e5d0

--- cereal/log.capnp
+++ cereal/log.capnp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:897f81aa32f3dcd1412fd0fe676fde83f41803c5921d343b2b0f7a7cb1c8166f
-size 20745
+oid sha256:7bce96438909cf6db02b0f5debb3cb882cafd7bf2555a735d53c4abafa1b599d
+size 20774
--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd0662c705d1dba5a707ba7d961e65db3391d78dd788a979e6501e53e8c97e39
-size 13296
+oid sha256:4c9f0a3b146c49d23f54bee7ddb25d99e23bbc0dabc5488c0a279988d6d11cb5
+size 13425
