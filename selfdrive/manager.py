053a79355d51a6adc73e6a8499ae228794f7e6dd
Vehicle Researcher <user@comma.ai>
2018-05-23 03:59:04 +0000

    openpilot v0.4.6 release
    old-commit-hash: c318b27a2f82f02ef3d8eb885f379f8a9ef42265

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:360654b31377dfaad037ec907c017318abd0f6bf83126fe4c170a3b0bee67aca
-size 21339
+oid sha256:e136fa6ef54701e4a5d707373b67d807f0032cfeac7aed95afb277d7e0723844
+size 21926
