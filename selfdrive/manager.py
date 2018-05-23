5a2ff129c40900a97ba1c0bc5e5ce7870aa4ebac
Willem Melching <willem.melching@gmail.com>
2018-05-24 00:03:09 +0200

    clean git repo before neos update
    old-commit-hash: 79fb0621480ec5d57e844cf5c2b89368073c594d

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e136fa6ef54701e4a5d707373b67d807f0032cfeac7aed95afb277d7e0723844
-size 21926
+oid sha256:bc512b447720f1e0ecbfb3d6b97bb248116ea3989bd2f6d3aa949638a9b098e9
+size 22043
