6db1dc17314f41bedaa9caa8b19080be8e074c38
Vehicle Researcher <user@comma.ai>
2018-06-25 13:45:14 -0700

    Squashed 'panda/' changes from 0dcd84d7..e7ca587b
    e7ca587b long isotp msgs
    1a94543a Panda safety: minor generalization of the function max_limit_check
    6b316011 Safety: minor data type cleanup
    
    git-subtree-dir: panda
    git-subtree-split: e7ca587b2bf70ae321e0260a7997cf893f8c8389
    
    old-commit-hash: 773e5a740f80aa4f107e6e291a9e8fb2819bd603

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45e83235f834fe400ac06f0861530ea091e65114f629f6daec8dc9ec6c499f77
-size 5489
+oid sha256:19f8c4c3b86e3a79ba1a71c08b31fee8e03165ceb76b1ef19e124034351dd58c
+size 5509
