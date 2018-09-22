c16375b0b5e473669376714b94e5272a8a317bc0
Vasily Tarasov <vtarasov@zoneos.com>
2018-09-21 19:41:44 -0700

    Increase Volt's steering limit (#351)
    * Increase Volt's steering limit
    
    * Merge panda subtree
    
    old-commit-hash: ba2118e9bfecfa7eec6aed051a8557841df81308

--- panda/tests/safety/test_gm.py
+++ panda/tests/safety/test_gm.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55ddc3e1bbb4c47807a4501755a4c5f2d952a323dfc3341474783e907c5773f4
+oid sha256:60b5ec0bfe6b776f1bbce0ed959aeaced18b00fc88608740f2ebcd4306fdbff3
 size 9439
