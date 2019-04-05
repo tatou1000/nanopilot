698094a0992732a372cb5385f9032971e326afc7
Riccardo <riccardo.biasini@gmail.com>
2019-04-05 00:27:39 -0700

    Chrysler: matching file in panda repo
    old-commit-hash: 5064aa0232462c251de4cab74d612200341df0bb

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:203e5c2e2ab6bed3f4df325ab4c4fcfcf079c8d8877760b267da4743c185bbf7
-size 4624
+oid sha256:51713b545610e24cbf3bb9af8647cb641313feb74d0f22ff95513cb1236568d5
+size 4655
