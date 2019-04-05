b30da57ff95734feac783ff99429e2e5ad1658a3
Drew Hintz <pushespretn@gmail.com>
2019-04-04 22:48:37 -0700

    📋📷 2019 Chrysler Pacifica and 2019 Jeep Grand Cherokee support (#590)
    * 2019 Chrysler Pacfica and 2019 Jeep Grand Cherokee support, along with likely support for arbitrary models.
    This is done by copying most values from the stock LKAS camera which is active with https://github.com/commaai/panda/pull/177
    
    * No longer send LKAS_HEARTBIT because Panda now forwards it for us.
    
    * Pacifica Hybrid 2018 combine fingerprints, add 808: 8
    
    * panda chrysler: forward bus 0 to bus 2
    copy of Panda commit: https://github.com/commaai/panda/commit/104950264473274e0012a39b7ea2d1468ef46d8d
    
    old-commit-hash: 8b36f861701062d40aa77928ab552c91dc095ecc

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bde3a81d35d7b7c1cbf5b27da0935564488c5972ee2fc38c45ef83b2d229f21f
-size 4090
+oid sha256:203e5c2e2ab6bed3f4df325ab4c4fcfcf079c8d8877760b267da4743c185bbf7
+size 4624
