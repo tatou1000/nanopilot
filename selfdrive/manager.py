892956aeb6095c196ac662b55936a54f31112d2c
Nigel Armstrong <legonigel@users.noreply.github.com>
2019-04-12 23:12:34 -0700

    Add pylint to travis-ci (#595)
    * Add pylint to travis-ci
    
    * Change travis build order
    
    * Fix missing pylint
    
    * Add pylint to dockerfile
    
    * Test with fixed linter for chrysler changes
    
    * Fix linter issues in OP
    
    * fixed linter on ford
    
    * Fix Riccardo's comments
    
    old-commit-hash: 577cf6664bc0676e15b3f63d0721c3c6c066e589

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55f5f1e980caa0f9bf28afa3ac5c361fa6899e9977682d1a10fd46ffa8ce1e06
-size 15647
+oid sha256:90b493125c879eb8f9ae6dfcf66b09d2cd151ac52e812e7d8e12d922606d973f
+size 15675
