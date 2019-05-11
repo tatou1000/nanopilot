aa5f6f78160ffa369677349ff3de8095d63094b9
Martin Lillepuu <martin@mlp.ee>
2019-05-11 13:42:19 +0300

    add Openpilot lane line indicators to Subaru LKAS HUD
    old-commit-hash: 80b12f20d743da3b6f1cd60b797c39f8d86b6c08

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33026b541dad4d73818f444cf867b4cb219e8710bcf8461209dc01c7b9606555
-size 4124
+oid sha256:baabdd1b543dc606097c53674375817126c8c2d7e0efd18908f209e951d0ac82
+size 4139
